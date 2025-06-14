d03fb57d2f359356b36ebeef5a6e62833c954a31
Maciel <macieldfaria@gmail.com>
2025-06-13 21:52:08 -0300

    workspace: update $PROFILE

--- workspace/windos_terminal/Microsoft.PowerShell_profile.ps1
+++ workspace/windos_terminal/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,6 @@
 oh-my-posh init pwsh --config "C:\Users\mdfaria\Documents\dev\workspace\windos_terminal\oh-my-posh themes\kushal.omp.json" | Invoke-Expression
+
+# Só roda o Winfetch se não estiver dentro do VSCode
+if (-not ($env:TERM_PROGRAM -eq "vscode")) {
+    & winfetch
+}
